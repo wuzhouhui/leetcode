97b8d0f426a2882ee11279c4466d54b5a52a33b4
wuzhouhui <wuzhouhui250@gmail.com>
2015-06-22 21:11:47 +0800

    add 131

--- 131_palindrome_partition.java
+++ 131_palindrome_partition.java
@@ -1,6 +1,6 @@
 /*
  * wrong answer because of misunderstanding of problem
- */
+ *
 import java.util.*;
 
 class Solution {
@@ -65,5 +65,55 @@ class Solution {
 
     public static void main(String[] args) {
         List<List<String>> list = new Solution().partition("aab");
+    }
+}
+*/
+
+import java.util.*;
+
+class Solution {
+    private LinkedList<List<String>>  result;
+    private LinkedList<String>        partitions;
+    public List<List<String>> partition(String s) {
+        result = new LinkedList<>();
+        if (s.length() == 0)
+            return(result);
+
+        partitions = new LinkedList<>();
+        do_partitioning(s, 0);
+        return(result);
+    }
+
+    private void do_partitioning(String s, int start) {
+        if (start >= s.length()) {
+            List<String> list = new LinkedList<>();
+            for (int i = 0; i < partitions.size(); i++)
+                list.add(partitions.get(i));
+            result.add(list);
+            return;
+        }
+
+        for (int i = 1; (start + i) <= s.length(); i++) {
+            if (is_palind(s, start, i)) {
+                String substr = s.substring(start, start + i);
+                partitions.add(substr);
+                do_partitioning(s, start + i);
+                partitions.removeLast();
+            }
+        }
+    }
+
+    private boolean is_palind(String s, int start, int length) {
+        for (int i = start; i < (start + length / 2); i++)
+            if (s.charAt(i) != s.charAt(2 * start + length - i - 1))
+                    return(false);
+        return(true);
+    }
+
+    public static void main(String[] args) {
+        List<List<String>> list = new Solution().partition("cbbbcc");
+        for (int i = 0; i < list.size(); i++)
+            System.out.println(list.get(i));
+
     }
 }
